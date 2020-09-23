01cc67216b53c3fd81d9f96b72715cd99fc4471f
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-23 17:07:05 -0300

    Updated css

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.getElementById("user").innerHTML = "Olá, " + name + "!";
 let test = prompt("Quer iniciar o teste?\nResponda sim para iniciar ou não para fechar a página.");
 
 if (test == "não") {
-    alert("Obrigada");
+    alert("Obrigada. Você pode fechar a janela!");
     prompt.close();
 }
 
@@ -19,7 +19,7 @@ if (question1 == "b") {
 
 let question2 = prompt("2. O que '2+1+5 retorna?\na- 8\nb- '2'+6\nc- 215?");
 
-if (question1 == "c") {
+if (question2 == "c") {
     document.getElementById("question2-correct").innerHTML = "Questão 2";
 } else {
     document.getElementById("question2-wrong").innerHTML = "Questão 2";
@@ -27,7 +27,7 @@ if (question1 == "c") {
 
 let question3 = prompt("3. Qual dos seguintes elementos não possui tag de fechamento?\na- img\nb- p\nc- h")
 
-if (question1 == "a") {
+if (question3 == "a") {
     document.getElementById("question3-correct").innerHTML = "Questão 3";
 } else {
     document.getElementById("question3-wrong").innerHTML = "Questão 3";
