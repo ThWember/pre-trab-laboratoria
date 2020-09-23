e5856089f1402a95c24a0a6ae5971bb7ed6d1023
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-23 14:35:35 -0300

    Updated index

---
+++ main.js
@@ -0,0 +1,9 @@
+let name = prompt("Coloque o seu nome");
+
+function insertName() {
+    document.getElementById("user").innerHTML = "xxxx";
+}
+
+let test = prompt("Coloque o número correspondente a sua resposta\n Quer iniciar o teste?");
+
+
