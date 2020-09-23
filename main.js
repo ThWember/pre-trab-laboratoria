031671568c5baedae40723a3dbeefe7bdd567cd1
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-23 15:03:35 -0300

    Updated main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let name = prompt("Coloque o seu nome");
 
-function insertName() {
-    document.getElementById("user").innerHTML = "xxxx";
-}
+document.getElementById("user").innerHTML = "Olá, " + name;
 
-let test = prompt("Coloque o número correspondente a sua resposta\n Quer iniciar o teste?");
+let test = prompt("Quer iniciar o teste?\nResponda sim para iniciar ou não para fechar a página.");
 
+let question1 = prompt("O que é CSS?\na- Linguagem de marcação\b- Folha de estilo\nc- Linguagem de programação")
 
