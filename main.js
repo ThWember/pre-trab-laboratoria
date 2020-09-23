4b69d3715cd237abfc67e387f935cf334c4fa1ae
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-09-23 15:27:10 -0300

    Updated index

--- main.js
+++ main.js
@@ -4,10 +4,27 @@ document.getElementById("user").innerHTML = "Olá, " + name;
 
 let test = prompt("Quer iniciar o teste?\nResponda sim para iniciar ou não para fechar a página.");
 
-let question1 = prompt("O que é CSS?\na- Linguagem de marcação\nb- Folha de estilo\nc- Linguagem de programação");
+let question1 = prompt("1. O que é CSS?\na- Linguagem de marcação\nb- Folha de estilo\nc- Linguagem de programação");
 
 if (question1 == "b") {
-    document.getElementById("question1").innerHTML = "Questão 1";
+    document.getElementById("question1-correct").innerHTML = "Questão 1";
+} else {
+    document.getElementById("question1-wrong").innerHTML = "Questão 1";
 }
 
-let question2 = prompt("O que "2"+1+5 retorna?\na- 8\nb- "2"+6\nc- 215?");
+let question2 = prompt("2. O que '2+1+5 retorna?\na- 8\nb- '2'+6\nc- 215?");
+
+if (question1 == "c") {
+    document.getElementById("question2-correct").innerHTML = "Questão 2";
+} else {
+    document.getElementById("question2-wrong").innerHTML = "Questão 2";
+}
+
+
+let question3 = prompt("3. Qual dos seguintes elementos não possui tag de fechamento?\na- img\nb- p\nc- h")
+
+if (question1 == "a") {
+    document.getElementById("question3-correct").innerHTML = "Questão 3";
+} else {
+    document.getElementById("question3-wrong").innerHTML = "Questão 3";
+}
